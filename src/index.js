850d816e21b1f2e14d80562eaa257ae2eba049c0
Ana Carolina <garcone.anaxx@gmail.com>
2021-09-02 20:12:52 -0300

    iniciando projeto 03

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AppFuncao, { Botao , Paragrafos } from './components/AppFuncao';
+import AppFuncao from './components/AppFuncao';
 
 ReactDOM.render(
   <React.StrictMode>
     <AppFuncao />
-    <Paragrafos />
-    <Botao nome='Click'/>
   </React.StrictMode>,
   document.getElementById('root')
 )
